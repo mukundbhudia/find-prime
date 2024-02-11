2f6a265d7dbb9d3cc9870bde508607ab8958a1cb
Mukund Bhudia <code@mukund.uk>
2024-02-11 14:19:08 +0000

    chore: clippy fixes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate num;
 extern crate rand;
 extern crate time;
 
-use std::io::Write;
 use time::now;
 
 use core::ops::{Rem, Shr};
@@ -16,12 +15,12 @@ fn find_r_and_d(i: BigUint) -> (u64, BigUint) {
     loop {
         if d.clone().rem(&2u64.to_biguint().unwrap()) == Zero::zero() {
             d = d.shr(1usize);
-            r = r + 1;
+            r += 1;
         } else {
             break;
         }
     }
-    return (r, d);
+    (r, d)
 }
 
 fn might_be_prime(n: &BigUint) -> bool {
@@ -35,13 +34,13 @@ fn might_be_prime(n: &BigUint) -> bool {
     let mut a: BigUint;
     'WitnessLoop: for _kk in 0..6u64 {
         a = rng.gen_biguint_range(&two, &nsub1);
-        x = mod_exp(&mut a, &mut d, &n);
-        if &x == &one || x == nsub1 {
+        x = mod_exp(&mut a, &mut d, n);
+        if x == one || x == nsub1 {
             continue;
         }
         for _rr in 1..r {
             x = (&x * &x) % n;
-            if &x == &one {
+            if x == one {
                 return false;
             } else if x == nsub1 {
                 continue 'WitnessLoop;
@@ -57,7 +56,7 @@ fn mod_exp(base: &mut BigUint, exponent: &mut BigUint, modulus: &BigUint) -> Big
     let zero = BigUint::zero();
     let mut result = BigUint::one();
 
-    while &*exponent > &zero {
+    while *exponent > zero {
         if &*exponent & &one == one {
             result = (result * &*base) % modulus;
         }
@@ -71,17 +70,11 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 3 {
-        writeln!(
-            std::io::stderr(),
-            "Usage: rusty-prime START MAX_PRIMES_TO_FIND"
-        )
-        .unwrap();
-        writeln!(
-            std::io::stderr(),
+        eprintln!("Usage: rusty-prime START MAX_PRIMES_TO_FIND");
+        eprintln!(
             "Example: {} 17 5. Will find 5 more primes after 17.",
             args[0]
-        )
-        .unwrap();
+        );
         std::process::exit(1);
     }
 
@@ -89,12 +82,7 @@ fn main() {
     let max_primes_to_find = &args[2];
 
     if start_prime == "1" {
-        writeln!(
-            std::io::stderr(),
-            "Please use a larger start prime than {}.",
-            start_prime
-        )
-        .unwrap();
+        eprintln!("Please use a larger start prime than {}.", start_prime);
         std::process::exit(1);
     }
 
@@ -110,7 +98,7 @@ fn main() {
         if let Ok(max_primes_to_find) = max_primes_to_find.parse::<i32>() {
             while i < max_primes_to_find {
                 let one = BigUint::one();
-                b = b + (&one + &one);
+                b += &one + &one;
                 if might_be_prime(&b) {
                     i += 1;
                     println!("{}", b);
